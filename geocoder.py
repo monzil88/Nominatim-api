9537df1a7c66ccbd542965b4a63b089e5b6662d2
monzil88 <Seawhale123!>
2021-07-03 17:59:15 -0500

    takes address list as input file

--- geocoder.py
+++ geocoder.py
@@ -1,4 +1,5 @@
 # packages
+import time
 import requests
 import json
 
@@ -21,17 +22,46 @@ class Geocoder:
         else:
             return None
 
-    def parse(self, res_in_json):
-        print(json.dumps(res_in_json, indent=2))
 
-    def run(self, input_address):
+    def address_list(self, input_file):
+
+        addresses = ''
+        # fetch from a file
+        with open(input_file, 'r') as f:
+            for line in f.read():
+                addresses += line
+        print(addresses)
+        # convert addresses into list
+        addresses = addresses.split('\n')
+        print(addresses)
+
+        return addresses
+
+    def parse(self, response_in_json):
+        print(json.dumps(response_in_json, indent=2))
+
+    def run(self, input_file):
+        # collect the address from the input fil
+        addresses = self.address_list(input_file)
+        for address in addresses:
+            res = self.fetch(address).json()
+            self.parse(response_in_json=res)
+
+            # adhere nominatim policy
+            time.sleep(2)
+
+    # Single Address search
         # WE need the response in json format
-        res_in_json = self.fetch(input_address).json()
+        # res_in_json = self.fetch(input_address).json()
         # print(res_in_json)
-        self.parse(res_in_json)
+        # self.parse(res_in_json)
+
+
+
+
 
 
 # main driver
 if __name__ == '__main__':
     geocoder = Geocoder()
-    geocoder.run(input_address=input("Type address\n"))
+    geocoder.run(input_file=input("Type filename\n"))
